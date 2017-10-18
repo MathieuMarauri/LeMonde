6afedb5ff2cbf2001f285678bcfdba8f8dad26b0
MathieuMarauri <mathieu.marauri@outlook.com>
2017-10-18 16:52:01 +0200

    lemmatize function is changed, no more pos removal

--- code/functions.R
+++ code/functions.R
@@ -111,19 +111,20 @@ createTopicTable <- function(topic, maxPage = 5, minPage = 1) {
 #' 
 #' @param text a raw character vectr of length one.
 #' @param id the id of the text in the corpus
+#' @param filter booena, should some pos be filtered out
 #' 
 #' @return a table with id, lemma and count
 #' 
-lemmatize <- function(text, id) {
+lemmatize <- function(text, id, filter = TRUE) {
   path_to_treetag <- 'C:/TreeTagger'
   writeLines(text = text, con = file.path(path_to_treetag, 'article_to_tag.txt'), useBytes = TRUE)
   shell('cd /d C:/TreeTagger & tag-french article_to_tag.txt tag_results.txt')
   tag_results <- fread(input = file.path(path_to_treetag, 'tag_results.txt'), header = FALSE, encoding = 'UTF-8', quote = "")
   names(tag_results) <- c('token', 'pos', 'lemma')
   file.remove(file.path(path_to_treetag, 'article_to_tag.txt'), file.path(path_to_treetag, 'tag_results.txt'))
-  tag_results <- tag_results[pos %in% c('ADJ', 'ADV', 'NOM') | stringi::stri_detect_regex(str = pos, pattern = '^VER')]
-  tag_results <- tag_results[!lemma %in% c('être', "avoir", "être/suivre")]
-  tag_results <- tag_results[, .(count = .N), by = lemma]
+  if(filter) {
+    tag_results <- tag_results[pos %in% c('ADJ', 'ADV', 'NOM', 'NAM') | stringi::stri_detect_regex(str = pos, pattern = '^VER')]
+  }
   tag_results <- cbind(id = id, tag_results)
   return(tag_results)
 }

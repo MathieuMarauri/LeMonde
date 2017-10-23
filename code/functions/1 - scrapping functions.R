7e0689e3f8e25426f8d4c837e0a70a38790ddf68
MathieuMarauri <mathieu.marauri@outlook.com>
2017-10-23 17:14:34 +0200

    functions file split

--- code/functions/1 - scrapping functions.R
+++ code/functions/1 - scrapping functions.R
@@ -106,26 +106,3 @@ createTopicTable <- function(topic, maxPage = 5, minPage = 1) {
                                        url, date_creation = Sys.time())]
   return(articles_topic)
 }
-
-#' This function lemmatizes a text using treetagger.
-#' 
-#' @param text a raw character vectr of length one.
-#' @param id the id of the text in the corpus
-#' @param filter booena, should some pos be filtered out
-#' 
-#' @return a table with id, lemma and count
-#' 
-lemmatize <- function(text, id, filter = TRUE) {
-  path_to_treetag <- 'C:/TreeTagger'
-  writeLines(text = text, con = file.path(path_to_treetag, 'article_to_tag.txt'), useBytes = TRUE)
-  shell('cd /d C:/TreeTagger & tag-french article_to_tag.txt tag_results.txt')
-  tag_results <- fread(input = file.path(path_to_treetag, 'tag_results.txt'), header = FALSE, encoding = 'UTF-8', quote = "")
-  names(tag_results) <- c('token', 'pos', 'lemma')
-  file.remove(file.path(path_to_treetag, 'article_to_tag.txt'), file.path(path_to_treetag, 'tag_results.txt'))
-  if(filter) {
-    tag_results <- tag_results[pos %in% c('ADJ', 'ADV', 'NOM', 'NAM') | stringi::stri_detect_regex(str = pos, pattern = '^VER')]
-  }
-  tag_results <- cbind(id = id, tag_results)
-  return(tag_results)
-}
-
